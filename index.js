c8b18027832344886460b83eb2da4b9af407bc0d
renanNixdorf <renanbomfim.dev@gmail.com>
2022-10-28 18:21:26 -0300

    interval

--- index.js
+++ index.js
@@ -19,11 +19,7 @@ const animationClass = "animate";
 function animationScroll() {
   const windowTop = window.pageYOffset + (window.innerHeight * 4) / 4;
   target.forEach((element) => {
-    if (windowTop > element.offsetTop) {
-      element.classList.add(animationClass);
-    } else {
-      element.classList.remove(animationClass);
-    }
+    if (windowTop > element.offsetTop) element.classList.add(animationClass);
   });
 }
 
