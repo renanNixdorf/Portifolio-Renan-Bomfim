ec0a5a415c682e905b97df01eceb58ec7a276d79
renanNixdorf <renanbomfim.dev@gmail.com>
2022-10-28 17:47:24 -0300

    JS animation

---
+++ index.js
@@ -0,0 +1,39 @@
+const debounce = function (func, wait, immediate) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const target = document.querySelectorAll("[data-animate]");
+const animationClass = "animate";
+
+function animationScroll() {
+  const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
+  target.forEach((element) => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  });
+}
+
+animationScroll();
+
+if (target.length) {
+  window.addEventListener(
+    "scroll",
+    debounce(() => {
+      animationScroll();
+    }, 200)
+  );
+}
